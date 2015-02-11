790d7b4c6bb13cd0112929cb7fb21d8e56933da7
Mathieu GRENET <mathieu@lesbouys.net>
2015-02-11 20:09:05 +0100

    Add licence header

--- MouseControl/FormConfig.xaml.cs
+++ MouseControl/FormConfig.xaml.cs
@@ -1,17 +1,30 @@
-﻿using Microsoft.Win32;
+﻿/*
+  MouseControl - Mouse Managment in multi DPI monitors environment
+  Copyright (c) 2015 Mathieu GRENET.  All right reserved.
+
+  This file is part of MouseControl.
+
+    ArduixPL is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ArduixPL is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with MouseControl.  If not, see <http://www.gnu.org/licenses/>.
+
+	  mailto:mathieu@mgth.fr
+	  http://www.mgth.fr
+*/
+
+using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 
 namespace MouseControl
 {

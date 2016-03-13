322f35639652071567f48a8ed9bdf2b3677acf9a
Mathieu GRENET <mathieu@lesbouys.net>
2016-03-13 09:14:46 +0100

    adjust refs

--- Argyll/ArgyllProbe.cs
+++ Argyll/ArgyllProbe.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.IO;
-using System.Linq;
 using System.Windows;
 using System.Xml.Serialization;
 using NotifyChange;
--- LittleBigMouse_Control/setup/LittleBigMouse.iss
+++ LittleBigMouse_Control/setup/LittleBigMouse.iss
@@ -1,6 +1,6 @@
 ; -- LittleBigMouse.iss --
 ;#define AppVer GetFileVersion('..\bin\x64\Release\LittleBigMouse_Control.exe')
-#define AppVer '3.0-alpha7'
+#define AppVer '3.0-alpha8'
 
 [Setup]
 AppName=Little Big Mouse
